2017d3a06f633403a3ee8bf77e9f8448065cb934
seven.si <Seven.Si@gtechdigi.com>
2022-07-08 17:48:58 +0800

    first

--- file/readFile.py
+++ file/readFile.py
@@ -1,7 +1,6 @@
-
 if __name__ == '__main__':
     with open('../data/new.json', mode='r', encoding='utf-8') as f:
         read = f.read()
-        print(read%("1","2","3","4","5"))
+        print(read % ("1", "2", "3", "4", "5"))
         # for line in f:
         #     print(line)
--- file/writeFile.py
+++ file/writeFile.py
@@ -1,5 +1,4 @@
-
 if __name__ == '__main__':
     with open('../data/tmp_order_order.txt', mode='w', encoding='utf-8') as fw:
         with open('../data/tmp_order_order.json', mode='r', encoding='utf-8') as fr:
-            fw.write(fr.read()%("7QsJ5Ww##s%yyDB","2%%%%%%%2"))
+            fw.write(fr.read() % ("7QsJ5Ww##s%yyDB", "2%%%%%%%2"))
